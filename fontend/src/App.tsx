72a3cd4858002b9e5c153dbd2d58ea5d3b49578d
foxmon <jjockrod@naver.com>
2022-10-05 17:01:51 +0900

    Fix: 경로 수정

--- fontend/src/App.tsx
+++ fontend/src/App.tsx
@@ -1,24 +1,14 @@
 import React from "react";
-import styled from "styled-components";
 import GlobalStyle from "@/styles/globalStyle";
-import GlobalTheme from "@/styles/theme";
-import Main from "@/pages/Main";
+import Main from "@/components/pages/Main";
+
 function App() {
   return (
     <React.Fragment>
       <Main />
-
       <GlobalStyle />
     </React.Fragment>
   );
 }
-// const CustomDiv = styled.div`
-//   font-family: ${GlobalTheme.fontStyle.regular};
-//   font-size: ${GlobalTheme.fontSize.moreBig};
-// `;
-
-// const CustomDiv2 = styled.div`
-//   font-family: ${GlobalTheme.fontStyle.bold};
-// `;
 
 export default App;
