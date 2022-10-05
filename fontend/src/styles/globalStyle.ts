eccdfde0fbe1e2d7112471df652e5e92a8c9b8cf
foxmon <jjockrod@naver.com>
2022-10-05 13:30:09 +0900

    initial setting

--- fontend/src/styles/globalStyle.ts
+++ fontend/src/styles/globalStyle.ts
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
 import EliceDigitalBaeumOTF_Bold from "@/styles/fonts/EliceDigitalBaeumOTF_Bold.otf";
 import EliceDigitalBaeumOTF_Regular from "@/styles/fonts/EliceDigitalBaeumOTF_Regular.otf";
 
 const GlobalStyle = createGlobalStyle`
+    ${normalize}
     @font-face {
         font-family: "eRegular";
         src: local("EliceDigitalBaeumOTF_Regular"), url(${EliceDigitalBaeumOTF_Regular}) format('opentype'); 
@@ -10,6 +12,20 @@ const GlobalStyle = createGlobalStyle`
     @font-face {
         font-family: "eBold";
         src: local("EliceDigitalBaeumOTF_Bold"), url(${EliceDigitalBaeumOTF_Bold}) format('opentype'); 
+    }
+    * { 
+        margin: 0; 
+        padding: 0; 
+    } 
+    html,
+    body, 
+    #root, 
+    .App { 
+        width: 100%; 
+        height: 100%; 
+        box-sizing: 
+        border-box; 
+        font-size: 62.5%; 
     }
 `;
 
--- fontend/src/styles/theme.ts
+++ fontend/src/styles/theme.ts
@@ -9,6 +9,17 @@ const GlobalTheme = {
     big: "2rem",
     moreBig: "3rem",
   },
+  colors: {
+    theme: "#6E41E2",
+    black: "#000000",
+    white: "#ffffff",
+    lightGray: "#FAFAFA",
+    gray: "#848484",
+  },
+  buttons: {
+    borderRadius: "4px",
+    border: "none",
+  },
 };
 
 export default GlobalTheme;
