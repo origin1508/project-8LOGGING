1f0b7de254bca2082f74e7b361977ce4973e091a
dlzagu <eodnsdlekd@naver.com>
2022-10-06 21:03:58 +0900

    feat: Profile 컴포넌트 생성

---
+++ frontend/src/components/profile/Profile.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+import styled from "styled-components";
+import User from "./User";
+import ChannelHistory from "./ChannelHistory";
+function Profile() {
+  return (
+    <ProfileContainer>
+      <User />
+      <ChannelHistory />
+    </ProfileContainer>
+  );
+}
+const ProfileContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 30rem;
+  height: 100%;
+  gap: 4rem;
+`;
+
+export default Profile;
