81603071c1e904a2dce0027c68dfaec614cf17ca
dlzagu <eodnsdlekd@naver.com>
2022-10-07 12:09:41 +0900

    feat:Profile api연결

--- frontend/src/components/pages/Profile.tsx
+++ frontend/src/components/pages/Profile.tsx
@@ -1,11 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { useNavigate, useParams } from "react-router-dom";
+import { isLoginState, curUserState } from "@/recoil/atoms/authState";
 import User from "../profile/User";
 import ChannelHistory from "../profile/ChannelHistory";
 import BasePageComponent from "@/components/hoc/BasePageComponent";
-
+import * as Api from "@/api/authFetcher";
 function Profile() {
+  const navigate = useNavigate();
+  const params = useParams();
   const [isEditing, setIsEditing] = useState<boolean>(false);
+  const isLogin = useRecoilValue(isLoginState);
+  const [curUser, setCurUser] = useRecoilState(curUserState);
+  const [isFetchCompleted, setIsFetchCompleted] = useState(false);
 
+  const fetchPorfolioOwner = async (curUserId: any) => {
+    const res = await Api.get("api/users", curUserId);
+    const curUserData = res.data.data;
+    setCurUser(curUserData);
+    setIsFetchCompleted(true);
+  };
+  useEffect(() => {
+    if (!isLogin) {
+      navigate("/login", { replace: true });
+      return;
+    }
+    if (params.userId) {
+      const curUserID = sessionStorage.getItem("userToken");
+      fetchPorfolioOwner(curUserID);
+    } else {
+      const curUserID = sessionStorage.getItem("userToken");
+      fetchPorfolioOwner(curUserID);
+    }
+  }, [params, navigate]);
   return (
     <BasePageComponent>
       <User isEditing={isEditing} setIsEditing={setIsEditing} />
