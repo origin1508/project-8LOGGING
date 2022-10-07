f9dbcbb8707c3b377b443ded61822482148f3f84
foxmon <jjockrod@naver.com>
2022-10-07 13:06:23 +0900

    Feat: ErrorContainer hoc

---
+++ frontend/src/components/hoc/BaseValidateTextContainer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import GlobalTheme from "@/styles/theme";
+import { BaseComponentType } from "@/types/common/baseComponentType";
+
+const BaseValidateTextContainer = ({ children }: BaseComponentType) => {
+  return <BaseContainer>{children}</BaseContainer>;
+};
+
+const BaseContainer = styled.div`
+  width: 100%;
+  font-size: ${GlobalTheme.fontSize.default};
+  color: ${GlobalTheme.colors.lightRed};
+`;
+
+export default BaseValidateTextContainer;
