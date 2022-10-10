e941d8acfac9b99700c9bbdbc26d6c541112df9e
dlzagu <eodnsdlekd@naver.com>
2022-10-10 15:14:51 +0900

    feat: usePsEditForm 커스텀 훅 생성

---
+++ frontend/src/hooks/usePsEditForm.ts
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { PsEditFormInitialType } from "@/types/auth/authTypes";
+import { ValidationType } from "@/types/auth/validationTypes";
+import ValidationUtil from "@/util/validationUtil";
+
+const usePsEditForm = (
+  initialState: PsEditFormInitialType
+): [
+  PsEditFormInitialType,
+  (e: React.ChangeEvent<HTMLInputElement>) => void,
+  ValidationType
+] => {
+  const [values, setValues] = useState(initialState);
+  const [isValid, setIsValid] = useState({
+    currentPassword: false,
+    newPassword: false,
+    confirmPassword: false,
+  });
+  const { checkPasswordValidate } = ValidationUtil;
+
+  useEffect(() => {
+    setIsValid({
+      currentPassword: checkPasswordValidate(values.currentPassword),
+      newPassword: checkPasswordValidate(values.currentPassword),
+      confirmPassword: checkPasswordValidate(values.currentPassword),
+    });
+  }, [values]);
+
+  const handleEditFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setValues({ ...values, [name]: value });
+  };
+
+  return [values, handleEditFormChange, isValid];
+};
+
+export default usePsEditForm;
