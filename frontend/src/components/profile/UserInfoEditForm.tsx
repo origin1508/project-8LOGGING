04da9747d423c4af9af20d6ca6553d9c03deaa01
dlzagu <eodnsdlekd@naver.com>
2022-10-10 16:49:13 +0900

    fix :타입삭제

--- frontend/src/components/profile/UserInfoEditForm.tsx
+++ frontend/src/components/profile/UserInfoEditForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled, { css } from "styled-components";
 import GlobalTheme from "@/styles/theme";
 import { curUserState } from "@/recoil/atoms/authState";
@@ -18,9 +18,7 @@ import {
 interface ImgProps {
   img?: string;
 }
-interface ButtonProps {
-  width?: string;
-}
+
 interface UserInfoEditProps {
   setIsEditing: React.Dispatch<React.SetStateAction<boolean>>;
   setIsPsEditing: React.Dispatch<React.SetStateAction<boolean>>;
@@ -52,10 +50,10 @@ function UserInfoEditForm({ setIsEditing, setIsPsEditing }: UserInfoEditProps) {
   const handleSubmitClick = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await Api.put(`/api/users/nickname`, {
+      const res = await Api.put("/api/users/nickname", {
         newNickname: values.nickname,
       });
-      const res2 = await Api.put(`/api/users/description`, {
+      const res2 = await Api.put("/api/users/description", {
         newDescription: values.description,
       });
 
@@ -120,7 +118,11 @@ function UserInfoEditForm({ setIsEditing, setIsPsEditing }: UserInfoEditProps) {
             CANCEL
           </EditButton>
         </EditButtonWrapper>
-        <EditButton width="60%" onClick={() => setIsPsEditing(true)}>
+        <EditButton
+          width="60%"
+          type="button"
+          onClick={() => setIsPsEditing(true)}
+        >
           비밀번호 변경하기
         </EditButton>
       </InpurForm>
