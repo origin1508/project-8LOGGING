4533de9a0fb4130e263bb06d75551178121bec9a
foxmon <jjockrod@naver.com>
2022-10-10 20:18:03 +0900

    Feat: MultiLineChart init

---
+++ frontend/src/components/chart/MultiLineChart.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { ChartProps } from "@/types/common/baseChartPropsType";
+import { chartRegistry, chartOptions } from "@/util/chartOption";
+import BaseChartContainer from "@/components/hoc/BaseChartContainer";
+import { Line } from "react-chartjs-2";
+
+chartRegistry();
+
+const MultiLineChart = ({ dataName, labels, multiDatas }: ChartProps) => {
+  const options = chartOptions("Korea sea monitor");
+
+  const dataForChartRender = () => {
+    const datasetLabel = [
+      "플라스틱 배출 속도 2050년까지 증가",
+      "플라스틱 배출 속도 2020년 수준 유지",
+      "플라스틱 배출 2020년에 중지",
+    ];
+    const borderColor = ["red", "blue", "green"];
+    const data: {
+      labels: string[];
+      datasets: Array<{
+        label: string;
+        data: Array<number>;
+        borderColor: string;
+        backgroundColor: string;
+      }>;
+    } = {
+      labels: labels,
+      datasets: [],
+    };
+    multiDatas?.forEach((d, i) => {
+      data.datasets.push({
+        label: `${dataName} ${datasetLabel[i]}`,
+        data: d,
+        borderColor: borderColor[i],
+        backgroundColor: borderColor[i],
+      });
+    });
+    return data;
+  };
+
+  return (
+    <BaseChartContainer>
+      <Line options={options} data={dataForChartRender()} />
+    </BaseChartContainer>
+  );
+};
+
+export default MultiLineChart;
