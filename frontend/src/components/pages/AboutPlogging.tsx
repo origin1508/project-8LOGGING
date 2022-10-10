581e2f382367115c2049ae2dea2a29071e4d86b4
foxmon <jjockrod@naver.com>
2022-10-10 21:30:40 +0900

    Fix: AboutPlogging

--- frontend/src/components/pages/AboutPlogging.tsx
+++ frontend/src/components/pages/AboutPlogging.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import GlobalTheme from "@/styles/theme";
 import useChartData from "@/hooks/useChartData";
 import BarChart from "@/components/chart/BarChart";
 import MultiLineChart from "@/components/chart/MultiLineChart";
@@ -24,6 +25,15 @@ const AboutPlogging = () => {
 
   return (
     <ChartContainer>
+      <AboutWrapper>
+        <AboutTitleContent>
+          플로깅Plogging이란 조깅을 하면서 길가의 쓰레기를 수거하는, 체육활동과
+          자연보호활동이 합쳐진 활동입니다. 2016년에 스웨덴에서 시작된 이
+          활동은, 2019년에 2백만 명의 사람들이 매일 플로깅에 참여하는 것으로
+          집계될 만큼 인기를 끌고 있습니다.
+        </AboutTitleContent>
+        <AboutImage src="/images/plogging-about-1.png" />
+      </AboutWrapper>
       <BarChart
         dataName="Korea Sea Monitor Bar"
         labels={barChartInfo?.labels}
@@ -40,6 +50,19 @@ const AboutPlogging = () => {
 
 const ChartContainer = styled.div`
   margin-left: 26rem;
+`;
+
+const AboutWrapper = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+const AboutTitleContent = styled.div`
+  font-size: ${GlobalTheme.fontSize.hyperBig};
+`;
+
+const AboutImage = styled.img`
+  width: 50%;
 `;
 
 export default AboutPlogging;
