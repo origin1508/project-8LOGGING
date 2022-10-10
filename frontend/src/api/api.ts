7349e493159a54aee289b9749c187e27b6b9fabd
dlzagu <eodnsdlekd@naver.com>
2022-10-10 11:47:22 +0900

    fix: import 삭제

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import Storage from "@/storage/storage";
-import { constSelector } from "recoil";
 
 const serverUrl = process.env.REACT_APP_SERVER_BASE_URL;
 interface ApiType {
