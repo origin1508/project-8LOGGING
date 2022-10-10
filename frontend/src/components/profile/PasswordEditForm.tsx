6c9bbca850a95639f076ddf1caf67258f646fbf1
dlzagu <eodnsdlekd@naver.com>
2022-10-10 11:45:40 +0900

    feat: passwordEditForm 생성

---
+++ frontend/src/components/profile/PasswordEditForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BaseCardContainer from "@/components/hoc/BaseCardContainer";
+import { BigTitle, TitleContainer } from "@/styles/commonStyle";
+function PasswordEditForm() {
+  return (
+    <BaseCardContainer>
+      <TitleContainer>
+        <BigTitle>Change Password</BigTitle>
+      </TitleContainer>
+    </BaseCardContainer>
+  );
+}
