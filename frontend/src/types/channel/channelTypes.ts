1fb3d839bdf427637584924aefe94572e904d6bb
foxmon <jjockrod@naver.com>
2022-10-11 14:54:30 +0900

    Fix: add channel type

--- frontend/src/types/channel/channelTypes.ts
+++ frontend/src/types/channel/channelTypes.ts
@@ -9,12 +9,21 @@ export interface ChannelFormInitialType {
 
 export interface ChannelsType {
   _id: string;
+  ownerId: string;
   title: string;
   ownerNickname: string;
   locationDist: string;
   locationCity: string;
   selectedCity: string;
-  imgUrl: string;
+  img: string;
   memberNum: number;
   curMemberNum: number;
+}
+
+export interface ChannelOwnerType {
+  _id?: string;
+  email: string;
+  nickname: string;
+  description: string;
+  profPic: string;
 }
