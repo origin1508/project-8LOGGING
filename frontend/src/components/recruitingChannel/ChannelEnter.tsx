8de046b14001cb207f989a2810222db717e3dacf
foxmon <jjockrod@naver.com>
2022-10-11 11:19:59 +0900

    Fix: ChannelEnter init

---
+++ frontend/src/components/recruitingChannel/ChannelEnter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const ChannelEnter = () => {
+  return <ChannelContainer></ChannelContainer>;
+};
+
+const ChannelContainer = styled.div`
+  width: 100%;
+`;
+
+export default ChannelEnter;
