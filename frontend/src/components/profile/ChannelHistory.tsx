7fe26cd6d343152b38adc90fa7a9cafd257ffbd6
dlzagu <eodnsdlekd@naver.com>
2022-10-12 23:38:56 +0900

    feat: 채널탭 생성

--- frontend/src/components/profile/ChannelHistory.tsx
+++ frontend/src/components/profile/ChannelHistory.tsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import GlobalTheme from "@/styles/theme";
 import ChannelHistoryCard from "./ChannelHistoryCard";
-
+import { SmallButton, BigTitle } from "@/styles/commonStyle";
+import * as Api from "@/api/api";
 function ChannelHistory() {
+  // const handleClickButton = async () => {
+  //   try {
+  //     const res = await Api.get("/api/users/channelhistory");
+  //     console.log(res.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+  const [tabIndex, setTabIndex] = useState(0);
+  const TapMenu = ["생성한 채널", "가입한 채널"];
   return (
     <ChannelHistoryContainer>
       <TitleContainer>
-        <Title>CHANNEL HISTORY</Title>
+        <BigTitle>Channel</BigTitle>
+        <ChannelTaps>
+          {TapMenu.map((menu, index) => {
+            return (
+              <Tab
+                onClick={() => {
+                  setTabIndex(index);
+                }}
+                key={index}
+                style={{
+                  borderBottom:
+                    tabIndex === index
+                      ? `2px solid ${GlobalTheme.colors.theme}`
+                      : "none",
+                  color:
+                    tabIndex === index
+                      ? GlobalTheme.colors.black
+                      : GlobalTheme.colors.gray,
+                }}
+              >
+                {menu}
+              </Tab>
+            );
+          })}
+        </ChannelTaps>
+        {/* <SmallButton onClick={handleClickButton}>api</SmallButton> */}
       </TitleContainer>
       <CardContainer>
         <ChannelHistoryCard />
@@ -20,6 +56,18 @@ function ChannelHistory() {
     </ChannelHistoryContainer>
   );
 }
+
+const ChannelTaps = styled.div`
+  display: flex;
+  margin-left: 2rem;
+  height: 3rem;
+  font-size: ${GlobalTheme.fontSize.littleBig};
+  margin-top: 3.8rem;
+`;
+const Tab = styled.div`
+  margin-right: 3rem;
+  cursor: pointer;
+`;
 
 const ChannelHistoryContainer = styled.div`
   overflow: hidden;
@@ -34,16 +82,12 @@ const ChannelHistoryContainer = styled.div`
 `;
 const TitleContainer = styled.div`
   padding: 2rem;
-  height: 20%;
+  height: 8rem;
   width: 100%;
   border-bottom: solid 1px ${GlobalTheme.colors.lightTwoGray};
   margin-bottom: 2rem;
 `;
-const Title = styled.h1`
-  margin-left: 2rem;
-  font-size: ${GlobalTheme.fontSize.moreBig};
-  font-family: ${GlobalTheme.fontStyle.bold};
-`;
+
 const CardContainer = styled.div`
   display: flex;
   justify-content: center;
