3c663dc5618c674201149800ad2c4d49b00c226b
foxmon <jjockrod@naver.com>
2022-10-12 10:38:25 +0900

    Fix: AboutPlogging 수정

--- frontend/src/components/pages/AboutPlogging.tsx
+++ frontend/src/components/pages/AboutPlogging.tsx
@@ -122,25 +122,6 @@ const AboutPlogging = () => {
 
   return (
     <ChartContainer>
-      <AboutWrapper>
-        <AboutTitleContent>
-          플로깅Plogging이란 조깅을 하면서 길가의 쓰레기를 수거하는, 체육활동과
-          자연보호활동이 합쳐진 활동입니다. 2016년에 스웨덴에서 시작된 이
-          활동은, 2019년에 2백만 명의 사람들이 매일 플로깅에 참여하는 것으로
-          집계될 만큼 인기를 끌고 있습니다.
-        </AboutTitleContent>
-        <AboutImage src="/images/plogging-about-1.png" />
-      </AboutWrapper>
-      <BarChart
-        dataName="해양 쓰레기"
-        labels={barChartInfo?.labels}
-        datas={barChartInfo?.data}
-      />
-      <MultiLineChart
-        dataName="미세플라스틱"
-        labels={multiLineChartInfo?.labels}
-        multiDatas={multiDatas}
-      />
       <Outer ref={outerDivRef}>
         <Dots scrollIndex={scrollIndex} />
         <AboutWrapper>
