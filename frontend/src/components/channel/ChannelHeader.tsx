9d2d09a8a584cac7ef214e84a0c3ac6e125d1183
origin1508 <origin1508@gmail.com>
2022-10-14 02:01:03 +0900

    Fix: 채널 스타일 수정

--- frontend/src/components/channel/ChannelHeader.tsx
+++ frontend/src/components/channel/ChannelHeader.tsx
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import GlobalTheme from "@/styles/theme";
 import { BigTitle, MediumSubTitle, MediumTitle } from "@/styles/commonStyle";
 import CustomIcon from "@/components/icons/CustomIcon";
-import MemberList from "./MemberList";
 
-const ChannelHeader = () => {
-  const [isShowMember, setIsShowMember] = useState(false);
-  const onModalCancleButtonClickEvent = () => setIsShowMember(false);
+interface PropsType {
+  title: string;
+  memberNums: number;
+  waitNums: number;
+}
+
+const ChannelHeader = ({ title, memberNums, waitNums }: PropsType) => {
   return (
     <TitleContainer>
       <BigTitle>Channel</BigTitle>
-      <MediumTitle>제주도에서 쓰레기 줍기</MediumTitle>
+      <MediumTitle>{title}</MediumTitle>
       <MediumSubTitle>
         <PeopleContainer>
           <CustomIcon
@@ -19,26 +22,10 @@ const ChannelHeader = () => {
             size="20"
             color={GlobalTheme.colors.gray}
           />
-          15
+          {memberNums}
         </PeopleContainer>
-        <Application>new applicant 3</Application>
+        <Application>new applicant {waitNums}</Application>
       </MediumSubTitle>
-      <NewPeopleContainer onClick={() => setIsShowMember(true)}>
-        <IconBox>
-          <CustomIcon
-            name="following"
-            size="30"
-            color={GlobalTheme.colors.theme}
-          ></CustomIcon>
-          <Notification>7</Notification>
-        </IconBox>
-
-        <NewPeople>Member</NewPeople>
-      </NewPeopleContainer>
-      <MemberList
-        isShowMember={isShowMember}
-        onModalCancleButtonClickEvent={onModalCancleButtonClickEvent}
-      ></MemberList>
     </TitleContainer>
   );
 };
@@ -68,53 +55,6 @@ const Application = styled.div`
   display: flex;
   align-items: center;
   gap: 0.5rem;
-`;
-
-const IconBox = styled.div`
-  position: relative;
-`;
-
-const NewPeopleContainer = styled.div`
-  position: absolute;
-  top: 4rem;
-  cursor: pointer;
-  right: 7rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-  &:hover {
-    transform: translateY(-0.3rem);
-  }
-  &::after {
-    transform: scale(1.5);
-    opacity: 0;
-  }
-  &:active {
-    transform: translateY(-0.1rem);
-  }
-`;
-
-const Notification = styled.span`
-  font-size: 0.8rem;
-  height: 1.75rem;
-  width: 1.75rem;
-  background-color: #eb2f64;
-  border-radius: 50%;
-  color: white;
-  position: absolute;
-  top: 1.2rem;
-  right: -0.4rem;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const NewPeople = styled.div`
-  font-weight: bold;
-  line-height: 1.4;
-  font-size: ${GlobalTheme.fontSize.littleBig};
 `;
 
 export default ChannelHeader;
--- frontend/src/components/channel/MemberList.tsx
+++ frontend/src/components/channel/MemberList.tsx
@@ -2,70 +2,101 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import GlobalTheme from "@/styles/theme";
+import CustomIcon from "@/components/icons/CustomIcon";
 import { TextOne, TextTwo, SmallButton, BigTitle } from "@/styles/commonStyle";
-import Modal from "../modal/Modal";
-
-interface UserContentType {
-  nickname: string;
-  email: string;
-  profPic: string;
-  _id: "string";
-}
+import { ChannelMemberType, waitListType } from "@/types/channel/channelTypes";
 
 interface MemberListProps {
-  isShowMember: boolean;
-  onModalCancleButtonClickEvent: () => void;
+  channelMemberList: ChannelMemberType[];
+  waitMemberList: waitListType[];
 }
 
 export default function MemberList({
-  isShowMember,
-  onModalCancleButtonClickEvent,
+  channelMemberList,
+  waitMemberList,
 }: MemberListProps) {
-  const [memberList, setMemberList] = useState<Array<UserContentType>>([]);
-
   const navigate = useNavigate();
 
   return (
-    <>
-      <Modal
-        isOpenModal={isShowMember}
-        isAlertModal={true}
-        isShowImage={false}
-        onModalCancelButtonClickEvent={onModalCancleButtonClickEvent}
-      >
-        <BigTitle>Member</BigTitle>
-        <UsersContainer>
-          <UserContainer>
-            <UserImg itemProp="https://cdn.imweb.me/upload/S20211110a3d216dc49446/f7bfffacbb6de.png"></UserImg>
-            <UserInfo>
-              <TextOne>들자구</TextOne>
-              <TextTwo>sdhjaksdj@naver.com</TextTwo>
-            </UserInfo>
-            <ButtonContainer>
-              <SmallButton>수락</SmallButton>
-              <SmallButton>거절</SmallButton>
-            </ButtonContainer>
-          </UserContainer>
-        </UsersContainer>
-      </Modal>
-    </>
+    <MemberListWrapper>
+      <MemberListContainer>
+        <BigTitle>Members</BigTitle>
+        <NewPeopleContainer>
+          <IconBox>
+            <CustomIcon
+              name="following"
+              size="30"
+              color={GlobalTheme.colors.theme}
+            ></CustomIcon>
+            <Notification>{waitMemberList.length}</Notification>
+          </IconBox>
+        </NewPeopleContainer>
+        {channelMemberList.map((data) => {
+          return (
+            <UserContainer key={data.memberId}>
+              <UserInfo onClick={() => navigate(`/profile/${data.memberId}`)}>
+                <UserImg itemProp={data.memberPic}></UserImg>
+                <TextOne>{data.memberNickname}</TextOne>
+              </UserInfo>
+              <SmallButton>follow</SmallButton>
+            </UserContainer>
+          );
+        })}
+      </MemberListContainer>
+
+      {waitMemberList &&
+        waitMemberList.map((data) => {
+          return (
+            <WaitListContainer key={data.userId}>
+              <BigTitle>Waiting Member</BigTitle>
+              <UserContainer>
+                <UserInfo onClick={() => navigate(`/profile/${data.userId}`)}>
+                  <UserImg itemProp={data.profPic}></UserImg>
+                  <TextOne>{data.nickname}</TextOne>
+                </UserInfo>
+                <ButtonContainer>
+                  <SmallButton>수락</SmallButton>
+                  <SmallButton>거절</SmallButton>
+                </ButtonContainer>
+              </UserContainer>
+            </WaitListContainer>
+          );
+        })}
+    </MemberListWrapper>
   );
 }
-
-const UsersContainer = styled.div`
-  width: 100%;
-  overflow-y: scroll;
-  height: 45rem;
-  border-radius: 1rem;
+const MemberListWrapper = styled.div`
+  position: relative;
+  width: 20%;
+  height: 65rem;
+  background-color: ${GlobalTheme.colors.white};
   display: flex;
   align-items: center;
   flex-direction: column;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 1rem;
+  margin-left: 2rem;
+`;
+
+const MemberListContainer = styled.div`
+  padding: 3rem;
+  width: 80%;
+  height: 100%;
+`;
+const WaitListContainer = styled.div`
+  display: none;
+  position: absolute;
+  padding: 3rem;
+  width: 80%;
+  height: 60%;
+  border-radius: 8px;
+  background-color: ${GlobalTheme.colors.lightTwoGray};
 `;
 const UserContainer = styled.div`
   margin-top: 2rem;
   display: flex;
-  width: 70%;
-  justify-content: space-between;
+  width: 100%;
+  justify-content: center;
   align-items: center;
   gap: 3rem;
   padding-bottom: 1rem;
@@ -80,10 +111,54 @@ const UserImg = styled.div`
 `;
 const UserInfo = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  align-items: center;
+  gap: 2rem;
+  cursor: pointer;
 `;
 
 const ButtonContainer = styled.div`
   display: flex;
   gap: 1rem;
+`;
+
+const IconBox = styled.div`
+  position: relative;
+`;
+
+const NewPeopleContainer = styled.div`
+  position: absolute;
+  top: 3.5rem;
+  cursor: pointer;
+  right: 3.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  &:hover {
+    transform: translateY(-0.3rem);
+  }
+  &::after {
+    transform: scale(1.5);
+    opacity: 0;
+  }
+  &:active {
+    transform: translateY(-0.1rem);
+  }
+`;
+
+const Notification = styled.span`
+  font-size: 0.8rem;
+  height: 1.75rem;
+  width: 1.75rem;
+  background-color: #eb2f64;
+  border-radius: 50%;
+  color: white;
+  position: absolute;
+  top: 1.2rem;
+  right: -0.4rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
