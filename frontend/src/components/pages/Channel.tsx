1ed3051e0261c355e081f9d3604c0fc69cb965b8
origin1508 <origin1508@gmail.com>
2022-10-14 02:00:37 +0900

    Feat: api 연동

--- frontend/src/components/pages/Channel.tsx
+++ frontend/src/components/pages/Channel.tsx
@@ -1,21 +1,48 @@
 import React, { useEffect, useState, useCallback } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { io } from "socket.io-client";
+import { currentChannelDetailRequest } from "@/api/channelFetcher";
+import { MainChannelType } from "@/types/channel/channelTypes";
+import Modal from "@/components/modal/Modal";
+import useModal from "@/hooks/useModal";
 import BaseCardContainerStyle from "@/components/hoc/BaseCardContainer";
 import BasePageComponent from "@/components/hoc/BasePageComponent";
 import GlobalTheme from "@/styles/theme";
 import ChannelHeader from "@/components/channel/ChannelHeader";
+import MemberList from "@/components/channel/MemberList";
 import ChannelSendButton from "@/components/channel/ChannelSendButton";
 import { TextOne, TextTwo } from "@/styles/commonStyle";
 
 function Channel() {
   const [channelContent, setChannelContent] = useState<string>("");
-
+  const [channelData, setChannelData] = useState<MainChannelType[]>([]);
+  const [entryFailureMessage, setEntryFailureMessage] = useState();
+  const { channelId } = useParams();
+  const navigate = useNavigate();
+  const [
+    isOpenModal,
+    ,
+    handleModalOpenButtonClick,
+    ,
+    handleModalCloseButtonClick,
+  ] = useModal(false);
   // const socket = io(`${process.env.REACT_APP_SERVER_BASE_URL}`);
 
   useEffect(() => {
-    // socket.emit("enter", "안녕하세요?");
-  }, []);
+    (async () => {
+      const res = await currentChannelDetailRequest(
+        `/api/channels/${channelId}/main`
+      );
+      if (res.success) {
+        setChannelData([res.datas]);
+      } else {
+        setChannelData([]);
+        handleModalOpenButtonClick();
+        setEntryFailureMessage(res.message);
+      }
+    })();
+  }, [channelId]);
 
   const handleChannelContentChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,24 +58,44 @@ function Channel() {
 
   return (
     <BasePageComponent>
-      <BaseCardContainerStyle width="100rem">
-        <ChannelHeader />
-        <ChatForm>
-          <ContentContainer>
-            <UserContainer>
-              <UserImg itemProp="https://cdn.imweb.me/upload/S20211110a3d216dc49446/f7bfffacbb6de.png"></UserImg>
-              <UserInfo>
-                <TextOne>들자구 </TextOne>
-                <TextTwo>ㅁㄴㅇㅁㄴㅇ</TextTwo>
-              </UserInfo>
-            </UserContainer>
-          </ContentContainer>
-          <ChatInput onChange={handleChannelContentChange} />
-          <ChannelSendButton
-            onChannelSendButtonEvent={handleChannelSendButtonClick}
-          />
-        </ChatForm>
-      </BaseCardContainerStyle>
+      {channelData &&
+        channelData.map((data) => {
+          return (
+            <React.Fragment key={data._id}>
+              <BaseCardContainerStyle width="100rem">
+                <ChannelHeader
+                  title={data.title}
+                  memberNums={data.membersInfo.length}
+                  waitNums={data.waitList.length}
+                />
+                <ChatForm>
+                  <ContentContainer>
+                    <UserContainer></UserContainer>
+                  </ContentContainer>
+                  <ChatInput onChange={handleChannelContentChange} />
+                  <ChannelSendButton
+                    onChannelSendButtonEvent={handleChannelSendButtonClick}
+                  />
+                </ChatForm>
+              </BaseCardContainerStyle>
+              <MemberList
+                channelMemberList={data.membersInfo}
+                waitMemberList={data.waitList}
+              />
+            </React.Fragment>
+          );
+        })}
+      <Modal
+        isOpenModal={isOpenModal}
+        isAlertModal={true}
+        isShowImage={true}
+        onModalCancelButtonClickEvent={() => {
+          handleModalCloseButtonClick;
+          navigate("/profile", { replace: true });
+        }}
+      >
+        {entryFailureMessage}
+      </Modal>
     </BasePageComponent>
   );
 }
