9ac01bc4b94ce597115e220f5ee612e564a5a716
foxmon <jjockrod@naver.com>
2022-10-16 23:36:58 +0900

    Fix: AboutPlogging

--- frontend/src/components/pages/AboutPlogging.tsx
+++ frontend/src/components/pages/AboutPlogging.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import useScroll from "@/hooks/useScroll";
 import useChartData from "@/hooks/useChartData";
