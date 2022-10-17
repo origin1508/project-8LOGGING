23d407dc71cda796b2b1b62c28a8b856c935a980
HyunSeong <hhs950120@gmail.com>
2022-10-17 13:43:12 +0900

    FIX: 다시 되돌려..

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -1,5 +1,4 @@
 const socket = require("socket.io");
-const { chatService } = require("../services");
 
 const socketConfig = (server, app) => {
   const io = socket(server, { path: "/chat-socket" });
@@ -14,41 +13,11 @@ const socketConfig = (server, app) => {
       socket.join(data.roomId);
     });
 
-    socket.on("create-chat", async (data) => {
-      console.log(data);
-
-      const createdChat = await chatService.addChatLog(data.roomId, data.userId, data.chat);
-      const userChatInfo = await chatService.getUserChatLog(createdChat._id);
-
-      console.log(userChatInfo);
-
-      socket.emit("chat", userChatInfo);
-    });
-
-    socket.on("modify-chat", async (data) => {
-      console.log('수정 데이터');
-      console.log(data);
-
-      await chatService.updateChatLog(data.chatId, data.chat);
-      const userChatInfo = await chatService.getUserChatLog(data.chatId);
-
-      socket.emit("chat", userChatInfo);
-    })
-
-    socket.on("remove-chat", async (data) => {
-      console.log("삭제 데이터");
-      console.log(data);
-
-      const userChatInfo = await chatService.getUserChatLog(data.chatId);
-      await chatService.deleteChatLog(data.chatId, data.roomId);
-
-      socket.emit("chat", userChatInfo);
-    })
-
     socket.on("disconnect", (data) => {
       console.log(data.roomId + "에서 연결 해제");
       socket.leave(data.roomId);
     });
+
   });
 };
 
