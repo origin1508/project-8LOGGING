185306fdcf2d8232c9d7417edf42a858692d7f29
HyunSeong <hhs950120@gmail.com>
2022-10-17 22:03:58 +0900

    FIX: 소켓 재수정

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -13,8 +13,9 @@ const socketConfig = (server) => {
       console.log(data.roomId + "에 접속");
       socket.roomId = data.roomId;
       const chatLog = await chatService.getChatLog(socket.roomId);
+      console.log(chatLog);
       socket.join(socket.roomId);
-      socket.to(socket.roomId).emit('receive-chatLog', chatLog);
+      socket.emit('receive-chatLog', chatLog);
     });
 
     socket.on("create-chat", async (data) => {
