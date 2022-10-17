a38595b1015c9f3cdecc605653c990e5b2461bab
HyunSeong <hhs950120@gmail.com>
2022-10-17 17:56:49 +0900

    FEAT: swagger data api docs 추가

--- backend/src/routes/data.route.js
+++ backend/src/routes/data.route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { dataCtrl } = require("../controllers");
 
-router.get("/:dataName", dataCtrl.loadData);
+router.get("/:dataName", dataCtrl.loadData); // 분석한 데이터 불러오기
 
 module.exports = router;
