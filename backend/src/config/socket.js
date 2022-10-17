035f30e9b6b2421def5fc4c5253a838fb46cb044
HyunSeong <hhs950120@gmail.com>
2022-10-17 11:58:15 +0900

    FIX: 채팅 추가

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -1,4 +1,5 @@
 const socket = require("socket.io");
+const { chatService } = require("../services");
 
 const socketConfig = (server, app) => {
   const io = socket(server, { path: "/chat-socket" });
@@ -13,11 +14,21 @@ const socketConfig = (server, app) => {
       socket.join(data.roomId);
     });
 
+    socket.on("create-chat", async (data) => {
+      console.log(data);
+
+      const createdChat = await chatService.addChatLog(data.roomId, data.userId, data.chat);
+      const userChatInfo = await chatService.getUserChatLog(createdChat._id);
+
+      console.log(userChatInfo);
+
+      socket.emit("chat", userChatInfo);
+    });
+
     socket.on("disconnect", (data) => {
       console.log(data.roomId + "에서 연결 해제");
       socket.leave(data.roomId);
     });
-
   });
 };
 
