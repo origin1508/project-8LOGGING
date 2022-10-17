a8138cb71940ddc3b40b0e643af01931bcef939a
HyunSeong <hhs950120@gmail.com>
2022-10-17 17:52:07 +0900

    FEAT: swagger chat api docs 추가

--- backend/src/routes/chat.route.js
+++ backend/src/routes/chat.route.js
@@ -4,10 +4,10 @@ const jwtVerification = require("../middlewares/jwtVerification");
 
 const router = express.Router();
 
-router.post("/room", jwtVerification, chatCtrl.createChatRoom);
-router.get("/log/:roomId", jwtVerification, chatCtrl.showChatLog);
-router.post("/log", jwtVerification, chatCtrl.createChatLog);
-router.put("/log", jwtVerification, chatCtrl.modifyChatLog);
-router.delete("/log", jwtVerification, chatCtrl.removeChatLog);
+router.post("/room", jwtVerification, chatCtrl.createChatRoom); // 채팅방 생성
+router.get("/log/:roomId", jwtVerification, chatCtrl.showChatLog); // 모든 채팅 로그 조회
+router.post("/log", jwtVerification, chatCtrl.createChatLog); // 채팅 로그 생성
+router.put("/log", jwtVerification, chatCtrl.modifyChatLog); // 채팅 로그 수정
+router.delete("/log", jwtVerification, chatCtrl.removeChatLog); // 채팅 로그 삭제
 
-module.exports = router;
+module.exports = router; 
