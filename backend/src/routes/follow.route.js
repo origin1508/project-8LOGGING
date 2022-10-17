add44ec944d647458b335c0d930b1b9ec0e0973b
HyunSeong <hhs950120@gmail.com>
2022-10-17 18:09:02 +0900

    FEAT: swagger follow api docs 추가

--- backend/src/routes/follow.route.js
+++ backend/src/routes/follow.route.js
@@ -4,9 +4,9 @@ const { followCtrl } = require("../controllers");
 
 const router = express.Router();
 
-router.get("/list/:userId", jwtVerification, followCtrl.showFollowList);
-router.get("/:targetId", jwtVerification, followCtrl.checkFollow);
-router.post("/", jwtVerification, followCtrl.addFollow);
-router.delete("/", jwtVerification, followCtrl.removeFollow);
+router.get("/list/:userId", jwtVerification, followCtrl.showFollowList); // 팔로우 리스트 조회
+router.get("/:targetId", jwtVerification, followCtrl.checkFollow); // 팔로우 유무 체크
+router.post("/", jwtVerification, followCtrl.addFollow); // 팔로우 추가
+router.delete("/", jwtVerification, followCtrl.removeFollow); // 팔로우 삭제
 
 module.exports = router;
