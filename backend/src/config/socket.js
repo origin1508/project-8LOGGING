5c35eb9e9e40b5b60ba16d231b43c4ccac21e128
HyunSeong <hhs950120@gmail.com>
2022-10-17 21:41:26 +0900

    FIX: http 통신 없이 socket 통신하기

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -1,23 +1,49 @@
 const socket = require("socket.io");
+const { chatService } = require("../services");
 
-const socketConfig = (server, app) => {
+const socketConfig = (server) => {
   const io = socket(server, { path: "/chat-socket" });
-  app.set("chatIO", io);
   const chat = io.of("/chat");
 
   chat.on("connection", (socket) => {
-    console.log("chat 연결");
+    console.log(`chat 연결 ${socket.id}`);
+    
 
     socket.on("enter", (data) => {
       console.log(data.roomId + "에 접속");
+      socket.roomId = data.roomId;
       socket.join(data.roomId);
     });
 
-    socket.on("disconnect", (data) => {
-      console.log(data.roomId + "에서 연결 해제");
-      socket.leave(data.roomId);
-    });
+    socket.on("create-chat", async (data) => {
+      const createdChat = await chatService.addChatLog(
+        socket.roomId,
+        data.userId,
+        data.chat
+      );
+      const userChatInfo = await chatService.getUserChatLog(createdChat._id);
 
+      socket.to(socket.roomId).emit("receive-create-chat", userChatInfo);
+    }); // data = {userId, roomId, chat}
+
+    socket.on("modify-chat", async (data) => {
+      await chatService.updateChatLog(data.chatId, data.chat);
+      const userChatInfo = await chatService.getUserChatLog(data.chatId);
+
+      socket.to(socket.roomId).emit("receive-modify-chat", userChatInfo);
+    }); // data = {chatId, roomId, chat}
+
+    socket.on("remove-chat", async (data) => {
+      const userChatInfo = await chatService.getUserChatLog(data.chatId);
+      await chatService.deleteChatLog(data.chatId, socket.roomId);
+
+      socket.to(socket.roomId).emit("receive-remove-chat", userChatInfo);
+    }); // data = {chatId, roomId}
+
+    socket.on("disconnect", () => {
+      console.log(socket.id + "에서 연결 해제"); 
+      socket.leave(socket.roomId);
+    });
   });
 };
 
