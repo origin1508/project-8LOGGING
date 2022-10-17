e7850aa6b4a0aea57b6732a0746c4437421e88c3
HyunSeong <hhs950120@gmail.com>
2022-10-17 22:45:06 +0900

    FEAT: enter-chat 수정

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -15,8 +15,7 @@ const socketConfig = (server) => {
       const chatLog = await chatService.getChatLog(socket.roomId);
       console.log(chatLog);
       socket.join(socket.roomId);
-      chat.to(socket.roomId).emit('receive-chatLog', chatLog);
-      
+      socket.to(socket.roomId).emit('receive-chatLog', chatLog);
     });
 
     socket.on("create-chat", async (data) => {
