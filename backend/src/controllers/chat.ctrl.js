6cac8a7d3f43ded70ff0a7f5c9a9d7989dbf3ffe
HyunSeong <hhs950120@gmail.com>
2022-10-17 14:20:30 +0900

    FIX: 소켓 create, modify, remove emit 생성

--- backend/src/controllers/chat.ctrl.js
+++ backend/src/controllers/chat.ctrl.js
@@ -27,7 +27,7 @@ module.exports = {
       const createdChat = await chatService.addChatLog(roomId, userId, chat);
       const userChatInfo = await chatService.getUserChatLog(createdChat._id);
 
-      req.app.get("chatIO").of("/chat").to(roomId).emit("chat", userChatInfo);
+      req.app.get("chatIO").of("/chat").to(roomId).emit("create-chat", userChatInfo);
 
       res.status(201).json({
         success: true,
@@ -48,7 +48,7 @@ module.exports = {
 
       const userChatInfo = await chatService.getUserChatLog(chatId);
 
-      req.app.get("chatIO").of("/chat").to(roomId).emit("chat", userChatInfo);
+      req.app.get("chatIO").of("/chat").to(roomId).emit("modify-chat", userChatInfo);
 
       res.status(201).json({
         success: true,
@@ -69,7 +69,7 @@ module.exports = {
 
       await chatService.deleteChatLog(chatId, roomId);
 
-      req.app.get("chatIO").of("/chat").to(roomId).emit("chat", userChatInfo);
+      req.app.get("chatIO").of("/chat").to(roomId).emit("remove-chat", userChatInfo);
 
       res.status(201).json({
         success: true,
