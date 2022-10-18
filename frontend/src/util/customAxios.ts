1003b40237ff5cbfa033e602278125c0fdd6106b
foxmon <jjockrod@naver.com>
2022-10-18 21:38:19 +0900

    Fix: customAxios fix

--- frontend/src/util/customAxios.ts
+++ frontend/src/util/customAxios.ts
@@ -1,4 +1,6 @@
 import axios, { AxiosError } from "axios";
+import Storage from "@/storage/storage";
+import CookieStorage from "@/storage/cookie";
 
 const baseUrl = process.env.REACT_APP_SERVER_BASE_URL;
 
@@ -25,6 +27,29 @@ customAxios.interceptors.response.use(
   },
   (error) => {
     if (error.response.status >= 400 && error.response.status < 500) {
+      const { statusText, data } = error.response;
+      const { neededBothToken } = data;
+      if (statusText === "Unauthorized") {
+        if (neededBothToken) {
+          Storage.clearToken();
+          CookieStorage.clearToken();
+          window.location.replace("/auth");
+        }
+        (async () => {
+          const res = await customAxios.post(
+            "/api/auth/refresh",
+            {},
+            {
+              headers: {
+                Authorization: `Bearer ${Storage.getToken()}`,
+                "refresh-token": CookieStorage.getToken(),
+              },
+            }
+          );
+          const { datas } = res.data;
+          Storage.setToken(datas.newAccessToken);
+        })();
+      }
       return Promise.reject(error);
     } else if (error.response.status >= 500) {
       return AxiosError;
