1730b29adbad690ff5e26573b66b3b53d00f97c9
HyunSeong <hhs950120@gmail.com>
2022-10-20 21:45:47 +0900

    소켓 수정

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -14,7 +14,7 @@ const socketConfig = (server) => {
       socket.roomId = data.roomId;
       const chatLog = await chatService.getChatLog(socket.roomId);
       socket.join(socket.roomId);
-      chat.to(socket.roomId).emit("receive-chatLog", chatLog);
+      socket.emit("receive-chatLog", chatLog);
     });
 
     socket.on("create-chat", async (data) => {
